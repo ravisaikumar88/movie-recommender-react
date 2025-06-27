b67fa31046dcae508e530974da09670f6b533637
TELU RAVI SAI KUMAR <105939078+ravisaikumar88@users.noreply.github.com>
2025-06-27 11:44:05 +0530

    Update Api

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "https://movie-recommender-react-production.up.railway.app/api",
+const API = axios.create({
+  baseURL: "http://127.0.0.1:8000/api", // FastAPI base
 });
+
+export default API;
